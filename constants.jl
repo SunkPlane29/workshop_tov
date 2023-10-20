c757385625167208450b1aec93c97ce169350465
André <andrezito300@gmail.com>
2023-10-20 09:22:32 -0300

    fix constants.jl and add todo

--- constants.jl
+++ constants.jl
@@ -9,12 +9,14 @@
 #
 # p0 = 100MeVfm3_to_pu
 
+#TODO: add units comment
+
 c = G = MSOLAR = 1
 
 si_c        = 2.99792458e8     # m s^(-1)
-si_G        = 6.67430          # m^3 kg^(-1) s^(-2)
-si_MSOLAR   = 1.98855e30  # kg
-si_ħ        = 6.582119569e-16 # eV s 
+si_G        = 6.67430e-11      # m^3 kg^(-1) s^(-2)
+si_MSOLAR   = 1.98855e30       # kg
+si_ħ        = 6.582119569e-16  # eV s 
 
 # convertion factors for c = G = MSOLAR = 1 system to SI units
 # mu, lu, tu are shorthands for mass unit, length unit and time unit
@@ -24,7 +26,7 @@ lu = si_G*si_MSOLAR*si_c^(-2)
 tu = si_G*si_MSOLAR*si_c^(-3)
 kg = mu^(-1)
 m  = lu^(-1)
-s  = lu^(-1)
+s  = tu^(-1)
 
 #pressure unit and pascal
 pu = mu*lu^(-1)*tu^(-2)
@@ -34,17 +36,17 @@ pa = pu^(-1)
 ħc = (si_c*10^(15))*(si_ħ*10^(-6))
 
 fm4_to_mevfm3 = ħc
-mevfm3_to_fm4 = ħc^(-1)
+mevfm3_to_fm4 = fm4_to_mevfm3^(-1)
 fm4_to_mev4 = ħc^4
-mev4_to_fm4 = ħc^(-4)
+mev4_to_fm4 = fm4_to_mev4^(-1) 
 mev4_to_mevfm3 = ħc^(-3)
-mevfm3_to_mev4 = ħc^3
+mevfm3_to_mev4 = mev4_to_mevfm3^(-1)
 
 ev_to_j = 1.602176634e-19
-j_to_ev = (1.602176634e-19)^(-1)
+j_to_ev = ev_to_j^(-1)
 
-mev4_to_jm3 = ((si_ħ*10^(-6))*si_c)^(-3) * (EV_TO_JOULE*10^6)
-jm3_to_mev4 = mev4_to_j^(-1)
+mev4_to_jm3 = ((si_ħ*10^(-6))*si_c)^(-3) * (ev_to_j*10^6)
+jm3_to_mev4 = mev4_to_jm3^(-1)
 
 MeVfm3 = mevfm3_to_mev4*mev4_to_jm3*pa
 pu_to_mevfm3 = MeVfm3^(-1)
